1e217a938cb5592fd579df8bb336cfcf6f24338f
Blake La Pierre <blakelapierre@gmail.com>
2017-08-30 23:33:35 -0400

    Create CNAME file for GitHub pages

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 preact build --no-prerender && \
 
-cd build && \
-git init && \
-git checkout -b gh-pages \
+echo "fact.company" >> build/CNAME && \
+
+cd build \
+&& git init \
+&& git checkout -b gh-pages \
 && git add . \
 && git commit -m "build" \
 && git remote add origin git@github.com:blakelapierre/keccak-hashed-json-graph
